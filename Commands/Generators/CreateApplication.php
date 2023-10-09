4fec0a0dab21ded2733c687e5326277fec0ebf3a
YonisSavary <yonissavary@outlook.fr>
2023-10-09 14:42:05 +0200

    "applications" default value for config commands

--- Commands/Generators/CreateApplication.php
+++ Commands/Generators/CreateApplication.php
@@ -37,7 +37,7 @@ class CreateApplication extends Command
         print("Enabling new applications\n");
 
         $config = Configuration::getInstance();
-        $config->edit("applications", function($applications) use ($values) {
+        $config->edit("applications", function($applications=[]) use ($values) {
             array_push($applications, ...$values);
             return array_values(array_unique($applications));
         });
