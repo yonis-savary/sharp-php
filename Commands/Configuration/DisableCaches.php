173015326f129b44abf28f992c02b792bd7bdd4f
yonis-savary <yonissavary@outlook.fr>
2024-09-26 16:44:41 +0200

    disable-caches command now clear cache files

--- Commands/Configuration/DisableCaches.php
+++ Commands/Configuration/DisableCaches.php
@@ -7,16 +7,23 @@ use Sharp\Classes\CLI\Command;
 use Sharp\Classes\Env\Configuration;
 use Sharp\Core\Autoloader;
 use Sharp\Classes\Core\Configurable;
+use Sharp\Commands\ClearCaches;
 
 class DisableCaches extends Command
 {
     public function getHelp(): string
     {
-        return "Disable every cache-able component";
+        return "Disable every cache-able component (use -k to keep existants cache files)";
     }
 
     public function __invoke(Args $args)
     {
+        if (!$args->isPresent("-k", "--keep-files"))
+        {
+            echo "Clearing all cache files...";
+            ClearCaches::execute("--all");
+        }
+
         $config = Configuration::getInstance();
         foreach (Autoloader::classesThatUses(Configurable::class) as $configurable)
         {
